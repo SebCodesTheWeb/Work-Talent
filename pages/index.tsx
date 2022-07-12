4b012e1a3cf86a1789c8696ee25099f044e3fb2c
Sebastian Delgado <sebastian.delgado@mediatool.com>
2022-07-12 17:32:55 +0200

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,151 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+import { 
+  Heading,
+  Center,
+  VStack,
+  HStack,
+  Input,
+  Text,
+  Box,
+  Button,
+  Select,
+  Textarea,
+} from '@chakra-ui/react'
+import { Formik } from 'formik'
+
+const Home: NextPage = () => {
+  return (
+  <Center pt={ 4 } h='100vh' bgColor="#333" color="#fff" >
+  <VStack>
+    <VStack>
+      <Heading >Job Talent</Heading>
+      <Text as="em">Write <strong>your</strong> resume</Text>
+    </VStack>
+     <Formik
+     initialValues={{ 
+       firstname: '', 
+       lastname: '', 
+       location: '',
+       phone: '',
+       e_mail: '',
+       about: '',
+       image: '',
+       }}
+     onSubmit={(values, { setSubmitting }) => {
+       setTimeout(() => {
+         setSubmitting(false)
+       }, 400);
+     }}
+   >
+     {({
+       values,
+       errors,
+       touched,
+       handleChange,
+       handleBlur,
+       handleSubmit,
+       isSubmitting,
+       /* and other goodies */
+     }) => (
+       <form onSubmit={handleSubmit}>
+         <VStack 
+         alignItems="start" 
+         mt={ 4 } 
+         border="1px solid #fff" 
+         py={ 4 } 
+         px={ 16 }
+         borderRadius={ 8 }
+         width="600px"
+         spacing={ 4 }
+         >
+         <Heading as="h2" size="lg">Step 1: Basic Contact Info</Heading>
+
+           <label htmlFor="firstname" >First Name: </label>
+           <Input
+             name="firstname"
+             onChange={handleChange}
+             onBlur={handleBlur}
+             value={values.email}
+           />
+           
+           <label htmlFor="lastname" >Last Name: </label>
+           <Input
+             name="lastname"
+             onChange={handleChange}
+             onBlur={handleBlur}
+             value={values.email}
+           />
+
+           <label htmlFor="location" >Continent: </label>
+           <Select
+             name="location"
+             onChange={handleChange}
+             onBlur={handleBlur}
+             value={values.email}
+             placeholder="Select Location"
+           >
+             <option value="europe">Europe</option>
+             <option value="north-america">North America</option>
+             <option value="south-america">South America</option>
+             <option value="Oceania">Oceania</option>
+             <option value="Asia">Asia</option>
+             <option value="Africa">Africa</option>
+          </Select>
+
+           <label htmlFor="phone" >Phone Number: </label>
+           <Input
+             name="phone"
+             onChange={handleChange}
+             onBlur={handleBlur}
+             value={values.email}
+           />
+
+           <label htmlFor="e_mail" >Email Address: </label>
+           <Input
+             name="e_mail"
+             onChange={handleChange}
+             onBlur={handleBlur}
+             value={values.email}
+           />
+
+           <label htmlFor="about" > Short text about yourself and job title: </label>
+           <Textarea
+             name="about"
+             onChange={handleChange}
+             onBlur={handleBlur}
+             value={values.email}
+           />
+           
+           <label htmlFor="image" >Paste Image URL: </label>
+           <Input
+             name="image"
+             onChange={handleChange}
+             onBlur={handleBlur}
+             value={values.email}
+           />
+           <Box 
+             border="1px solid #fff" 
+             p={ 2 }
+             borderRadius={ 8 }
+           >
+             <Link type="submit" href={
+             { pathname: '/portfolio',
+             query: {...values},
+             }}>
+               Submit
+             </Link>
+           </Box>
+         </VStack>
+       </form>
+     )}
+   </Formik>
+  </VStack>
+  </Center>
+  )
+}
+
+export default Home
