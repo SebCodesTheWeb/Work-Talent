4783643bd29e0be0c73d5d48c4b7f2261c6635eb
Sebastian Delgado <sebastian.delgado@mediatool.com>
2022-09-25 21:08:40 +0200

    made public tab

--- pages/hub/[userId].tsx
+++ pages/hub/[userId].tsx
@@ -37,6 +37,12 @@ import {
   ModalBody,
   ModalCloseButton,
   useDisclosure,
+  Tabs,
+  Flex,
+  TabList,
+  TabPanels,
+  TabPanel,
+  Tab,
 } from '@chakra-ui/react'
 import uniqid from 'uniqid'
 
@@ -74,7 +80,7 @@ const initialValues = {
   imageSRCS: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 }
 
-const Home: NextPage = ({ portfolios }: any) => {
+const Home: NextPage = ({ portfolios, publicPortfolios }: any) => {
   const { user } = useContext(UserContext)
   const { isOpen, onOpen, onClose } = useDisclosure()
   const [portfolioName, setPortfolioName] = useState('')
@@ -139,76 +145,123 @@ const Home: NextPage = ({ portfolios }: any) => {
           h="800px"
           spacing={4}
         >
-          <Heading>Your Portfolios: </Heading>
-          <Modal isOpen={isOpen} onClose={onClose} isCentered={true}>
-            <ModalOverlay />
-            <ModalContent
-              bgColor="gray.700"
-              color="#fff"
-              border="1px solid #fff"
-            >
-              <ModalHeader>Portfolio Name</ModalHeader>
-              <ModalCloseButton />
-              <ModalBody>
-                <Input
-                  value={portfolioName}
-                  onChange={(e) => setPortfolioName(e.target.value)}
-                />
-              </ModalBody>
-              <ModalFooter>
-                <Button
-                  colorScheme="teal"
-                  mr={3}
-                  onClick={initializeUserPortfolio}
-                >
-                  Create
-                </Button>
-              </ModalFooter>
-            </ModalContent>
-          </Modal>
-          <SimpleGrid
-            columns={4}
-            h="550px"
-            gap={16}
-            py={4}
-            pr={16}
-            overflowY="scroll"
-          >
-            {portfolios.map((portfolio: any, index: number) => (
-              <Center
-                w="200px"
-                h="200px"
-                border="1px solid #fff"
-                key={`${user?.uid}-${portfolio.portfolioName}`}
-                borderRadius={8}
-              >
-                <VStack>
-                  <Heading size="md">{portfolio.portfolioName}</Heading>
-                  <Button
-                    mr={4}
-                    size="md"
-                    variant="ghost"
+          <Tabs colorScheme="#fff" variant="enclosed">
+            <TabList>
+              <Tab>Your Portfolios</Tab>
+              <Tab>Other{"'"}s Portfolios</Tab>
+            </TabList>
+            <TabPanels>
+              <TabPanel pt={ 8 } p={ 0 } >
+                <Heading>Your Portfolios: </Heading>
+                <Text>{portfolios.length === 0? 'It looks like you don\'t have any portfolio added yet!' : `Welcome  ${user?.displayName}, here you can, create, view and edit your portfolio webpages`}</Text>
+                <Modal isOpen={isOpen} onClose={onClose} isCentered={true}>
+                  <ModalOverlay />
+                  <ModalContent
+                    bgColor="gray.700"
+                    color="#fff"
                     border="1px solid #fff"
-                    _hover={{ color: 'gray.700', bgColor: '#fff' }}
-                    onClick={() => editPortfolio(index)}
                   >
-                    Edit
-                  </Button>
-                </VStack>
-              </Center>
-            ))}
-          </SimpleGrid>
-          <Button
-            alignSelf="end"
-            mr={4}
-            size="md"
-            variant="ghost"
-            border="1px solid #fff"
-            _hover={{ color: 'gray.700', bgColor: '#fff' }}
-            onClick={onOpen}
-          >
-            Add new portfolio
-          </Button>
+                    <ModalHeader>Portfolio Name</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                      <Input
+                        value={portfolioName}
+                        onChange={(e) => setPortfolioName(e.target.value)}
+                      />
+                    </ModalBody>
+                    <ModalFooter>
+                      <Button
+                        colorScheme="teal"
+                        mr={3}
+                        onClick={initializeUserPortfolio}
+                      >
+                        Create
+                      </Button>
+                    </ModalFooter>
+                  </ModalContent>
+                </Modal>
+                <SimpleGrid
+                  columns={4}
+                  h="500px"
+                  gap={16}
+                  mt={ 8 }
+                  pr={16}
+                  overflowY="scroll"
+                >
+                  {portfolios.map((portfolio: any, index: number) => (
+                    <Center
+                      w="200px"
+                      h="200px"
+                      border="1px solid #fff"
+                      key={`${user?.uid}-${portfolio.portfolioName}`}
+                      borderRadius={8}
+                    >
+                      <VStack>
+                        <Heading size="md">{portfolio.portfolioName}</Heading>
+                        <Button
+                          mr={4}
+                          size="md"
+                          variant="ghost"
+                          border="1px solid #fff"
+                          _hover={{ color: 'gray.700', bgColor: '#fff' }}
+                          onClick={() => editPortfolio(index)}
+                        >
+                          Edit
+                        </Button>
+                      </VStack>
+                    </Center>
+                  ))}
+                </SimpleGrid>
+                <Flex w="full" alignSelf="end" justifyContent="end">
+                <Button
+                  mr={4}
+                  size="md"
+                  variant="ghost"
+                  border="1px solid #fff"
+                  _hover={{ color: 'gray.700', bgColor: '#fff' }}
+                  onClick={onOpen}
+                >
+                  Add new portfolio
+                </Button>
+                </Flex>
+              </TabPanel>
+              <TabPanel pt={ 8 } p={ 0 }>
+                <Heading>Public portfolios</Heading>
+                <Text>This are portfolios that have been generated using Job Talent</Text>
+                <SimpleGrid
+                  columns={4}
+                  h="500px"
+                  gap={16}
+                  mt={ 8 }
+                  pr={16}
+                  overflowY="scroll"
+                >
+                  {publicPortfolios.map((portfolio: any) => (
+                    <Center
+                      w="200px"
+                      h="200px"
+                      border="1px solid #fff"
+                      key={`${uniqid()}`}
+                      borderRadius={8}
+                    >
+                      <VStack>
+                        <Heading size="md">{portfolio.portfolioName}</Heading>
+                        <Button
+                          mr={4}
+                          size="md"
+                          variant="ghost"
+                          border="1px solid #fff"
+                          _hover={{ color: 'gray.700', bgColor: '#fff' }}
+                        >
+                          View
+                        </Button>
+                      </VStack>
+                    </Center>
+                  ))}
+                </SimpleGrid>
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
         </VStack>
       </VStack>
     </Center>
@@ -224,9 +277,15 @@ export async function getServerSideProps({ params }: any) {
   const portfolioData = await getDocs(userQuery)
   const portfolios: any[] = []
   portfolioData.forEach((portfolio) => portfolios.push(portfolio.data()))
+
+  const publicPortfolioQuery = collection(db, 'test-users')
+  const publicPortfolioData = await getDocs(publicPortfolioQuery)
+  const publicPortfolios: any[] = []
+  publicPortfolioData.forEach((portfolio) => publicPortfolios.push(portfolio.data()))
   return {
     props: {
       portfolios,
+      publicPortfolios,
     },
   }
 }
